53796bb637f07268b440b0dd4cca47e8ba6541dc
Garrick Sewsankar <gsewsankar@ufl.edu>
2021-04-10 00:02:58 -0400

    final for project

--- client/src/App.js
+++ client/src/App.js
@@ -17,31 +17,104 @@ function App() {
       </div>
       
       <div className="Section1" id='section1'>
-        <Zoom><p>Hello World</p></Zoom>
+        <Zoom>
+          <div className="row">
+          <h1>Artificialy Intelligent Conciousness</h1>
+          <p>What is the Philosophy of Conciousness?</p>
+          <iframe 
+          width="560" 
+          height="315" 
+          src="https://www.youtube.com/embed/rHKwIYsPXLg" 
+          title="YouTube video player" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+          allowfullscreen>
+          </iframe>
+          </div>
+          <div>
+            <p>John Searle discusses his famous Chinese room argument which holds that a digital computer executing a program cannot be shown to have a 
+              "mind", "understanding" or "consciousness", regardless of how intelligently or human-like the program may 
+              make the computer behave.
+              </p>
+          </div>
+        </Zoom>
       </div>
 
       <div className="Section2" id='section2'>
-        <Zoom><p>Hello World</p></Zoom>
+        <Zoom>
+          <h1>Turing Test</h1>
+          <p>The Turing Test is a method of inquiry in artificial intelligence (AI) 
+            for determining whether or not a computer is capable of thinking like a human being. 
+            The test is named after Alan Turing, the founder of the Turing Test and an English computer scientist, 
+            cryptanalyst, mathematician and theoretical biologist.
+            </p>
+            <iframe 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/3wLqsRLvV-c" 
+            title="YouTube video player2" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen>
+            </iframe>
+          </Zoom>
       </div>
 
       <div className="Section3" id='section3'>
-        <Zoom><p>Hello World</p></Zoom>
+        <Zoom>
+        <h1>The Technological Singularity</h1>
+          <p>The technological singularity—also, simply, the singularity—is a 
+            hypothetical point in time at which technological growth becomes 
+            uncontrollable and irreversible, resulting in unforeseeable changes to 
+            human civilization.
+            </p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ye5C-zlCAyM" title="YouTube video player3" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </Zoom>
       </div>
       
       <div className="Section4" id='section4'>
-        <Zoom><p>Hello World</p></Zoom>
+        <Zoom>
+        <h1>Philosophy of Artificial Intelligence</h1>
+          <p>The philosophy of artificial intelligence attempts to answer such questions as follows:
+            <li>Can a machine act intelligently? Can it solve any problem that a person would solve by thinking?</li>
+            <li>Are human intelligence and machine intelligence the same? Is the human brain essentially a computer?</li>
+            <li>Can a machine have a mind, mental states, and consciousness in the same sense that a human being can? Can it feel how things are?</li>
+            </p>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/39EdqUbj92U" title="YouTube video player4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </Zoom>
       </div>
 
       <div className="Section5" id='section5'>
-        <Zoom><p>Hello World</p></Zoom>
+        <Zoom>
+          <h1>Financial System</h1>
+          <p>The lesson of history is that you do not get a sustained economic recovery as long as the financial system is in crisis.</p>
+          <iframe 
+          width="560" 
+          height="315" 
+          src="https://www.youtube.com/embed/bgPDW0ZpgJU" 
+          title="YouTube video player5" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+          allowfullscreen>
+          </iframe>
+          
+        </Zoom>
       </div>
       
       <div className="Section6" id='section6'>
-        <Zoom><p>Hello World</p></Zoom>
+        <Zoom>
+        <h1>Cryptocurrencies and Blockchain Technology</h1>
+          <p>A cryptocurrency (or “crypto”) is a digital currency that can be used to buy goods and services, but uses an online ledger with strong cryptography to secure online transactions. Much of the interest in these unregulated currencies is to trade for profit, with speculators at times driving prices skyward.</p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/SSo_EIwHSd4" title="YouTube video player6" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </Zoom>
       </div>
 
       <div className="Section7" id="section7">
-        <Zoom><p>Hello World</p></Zoom>
+        <Zoom>
+        <h1>Digital Art and NFTs</h1>
+          <p>A non-fungible token (NFT) is a unit of data on a digital ledger called a blockchain, where each NFT can represent a unique digital item, and thus they are not interchangeable. NFTs can represent digital files such as art, audio, videos, items in video games and other forms of creative work.</p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/zpROwouRo_M" title="YouTube video player7" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </Zoom>
       </div>
       
       <div className="Footer" id='footer'>
--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -7,13 +7,13 @@ function NavBar() {
     <Rotate top right>
     <div className="NavBar">
         <a href='#header'>Top</a>
-        <a href='#section1'>Section 1</a>
-        <a href='#section2'>Section 2</a>
-        <a href='#section3'>Section 3</a>
-        <a href='#section4'>Section 4</a>
-        <a href='#section5'>Section 5</a>
-        <a href='#section6'>Section 6</a>
-        <a href='#section7'>Section 7</a>
+        <a href='#section1'>AI Conciousness</a>
+        <a href='#section2'>Turing Test</a>
+        <a href='#section3'>Tech-Singularity</a>
+        <a href='#section4'>AI Philosophy</a>
+        <a href='#section5'>Financial System</a>
+        <a href='#section6'>TimeCoin</a>
+        <a href='#section7'>VR Art</a>
         <a href='#footer'>Bottom</a>
     </div>
     </Rotate>
