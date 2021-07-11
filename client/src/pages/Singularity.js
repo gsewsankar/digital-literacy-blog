55d7d115869dbf2908ec718778bff9f52247d370
Garrick Sewsankar <gsewsankar@ufl.edu>
2021-07-11 17:15:29 -0400

    fixed errors

--- client/src/pages/Singularity.js
+++ client/src/pages/Singularity.js
@@ -9,7 +9,7 @@ function Singularity() {
         The technological singularity is a possible future event where humans become fully
         merged with technology. This in some people's eyes is the next stage for human evolution.
         I am not sure about the future but it is an interesting idea. In my opinion the future looks
-        a lot more like A Brave New World by H.G. Wells. Eveyone is happy but they don't realize that they 
+        a lot more like A Brave New World by Aldous Huxley. Eveyone is happy but they don't realize that they 
         are being manipulated by higher powers, and if they do they don't really mind because they are happy.
         <h3>Leave YOUR Thoughts Down Below</h3>
         <Comments></Comments>
